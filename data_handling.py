015873adfc92cd314eb9ae6280f4e3f20cc5604e
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-02-16 08:26:05 -0800

    data handling logic done

--- data_handling.py
+++ data_handling.py
@@ -6,6 +6,7 @@ import time
 import joblib
 
 import settings
+import ml
 from distributions import generate_random
 
 
@@ -16,6 +17,11 @@ def uuid():
 def schema_pkl(uuid):
     return os.join(settings.data_folder,
                    "{}_schema.pkl".format(uuid))
+
+
+def dataset_pkl(uuid):
+    return os.join(settings.data_folder,
+                   "{}_dataset.pkl".format(uuid))
 
 
 def experiments_pkl(uuid):
@@ -41,6 +47,18 @@ def write_schema(uuid, schema):
     joblib.dump(schema, schema_pkl(uuid))
 
 
+def read_dataset(uuid):
+    try:
+        dataset = joblib.load(dataset_pkl(uuid))
+    except:
+        dataset = []
+    return dataset
+
+
+def write_dataset(uuid, dataset):
+    joblib.dump(dataset, dataset_pkl(uuid))
+
+
 def read_experiments(uuid):
     try:
         experiments = joblib.load(experiments_pkl(uuid))
@@ -51,14 +69,30 @@ def read_experiments(uuid):
     return experiments
 
 
-def write_experiments(uuid, schema):
-    joblib.dump(schema, experiments_pkl(uuid))
+def write_experiments(uuid, experiments):
+    joblib.dump(experiments, experiments_pkl(uuid))
+
+
+def read_experiment():
+    try:
+        ids = joblib.load(ids_pkl())
+    except:
+        ids = {}
+    return ids
+
+
+def write_experiment(ids):
+    joblib.dump(ids, ids_pkl())
 
 
 def store_experiment(experiment):
-    ids = joblib.load(ids_pkl())
+    ids = read_experiment()
     ids[experiment["_id"]] = experiment
-    joblib.dump(ids_pkl())
+    write_experiment(ids)
+
+
+def get_experiment(_id):
+    return read_experiment()[_id]
 
 
 def postprocess_experiment(schema, experiment):
@@ -92,17 +126,67 @@ def get_experiment(uuid):
 
 
 def update_dataset(uuid, _id, _obj):
-    # TODO
-    # read in dataset
-    # read in experiment
-    # append experiment to dataset
-    # read in schema
-    # remove cols not in schema
-    pass
+    dataset = read_dataset(uuid)
+    experiment = get_experiment(_id)
+    experiment[_obj] = _obj
+    dataset.append(experiment)
+    write_dataset(uuid, dataset)
 
 
-def sample_points(schema, num):
-    pass # TODO
+def sample_points(schema):
+    keys = sorted(schema.keys())
+    points = []
+    for k in keys:
+        v = schema[k]
+        points.append(generate_random(v['distribution'],
+                                      v['params'],
+                                      settings.random_points_for_smbo))
+    return np.array(points).T
+
+
+def point_to_experiment(schema, point):
+    keys = sorted(schema.keys())
+    return dict(zip(keys, point))
+
+
+def dataset_to_matrix(schema, dataset):
+    keys = sorted(schema.keys())
+    defaults = [schema[k]['default'] for k in keys]
+
+    train = []
+    target = []
+    for point in dataset:
+        row = []
+        point_has_data = False
+        for key, default in zip(keys, defaults):
+            if key in point:
+                point_has_data = True
+                row.append(point[key])
+            else:
+                row.append(default)
+
+        # ignoring data for old experiments,
+        # should probably prune
+        if point_has_data:
+            row.append()
+            target.append(point["_obj"])
+
+    return train, target
+
+
+def regenerate_points(uuid):
+    schema = read_schema(uuid)
+    dataset = read_dataset(uuid)
+    train, target = dataset_to_matrix(schema, dataset)
+    points = sample_points(schema)
+    scores, importances = ml.score_points(train, target, points)
+
+    # TODO save feature importances
+
+    # save top points
+    top_points = sorted(zip(scores, points))[:settings.keep_points]
+    experiments = map(point_to_experiment, top_points)
+    write_experiments(uuid, experiments)
 
 
 def regenerate_points_loop():
@@ -110,8 +194,7 @@ def regenerate_points_loop():
     while True:
         val = TQ.pop()
         if val is not None:
-            # TODO regenerate points
-            pass
+            regenerate_points(val)
         else:
             time.sleep(settings.loop_sleep)
 
--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ import os
 
 min_points_for_smbo = 50
 random_points_for_smbo = 10000
-local_points_for_smbo = 2000
+# local_points_for_smbo = 2000
+keep_points = 10
 data_folder = "data"
 queue_folder = "timestamp"
 queue_limit = 10
