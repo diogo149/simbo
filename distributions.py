0feb88e4a309624ef9e3fef1ebdeb7860954182c
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-02-15 17:04:31 -0800

    binary distribution

--- distributions.py
+++ distributions.py
@@ -1,4 +1,22 @@
+import numpy as np
 
 
-class BinaryDistribution(object):
-    pass
+class ChoiceDistribution(object):
+    def __init__(self, num_values):
+        self.num_values = num_values
+
+    def generate(self, points):
+        return np.random.randint(0,
+                                 self.num_values,
+                                 points)
+
+
+class BinaryDistribution(ChoiceDistribution):
+    def __init__(self):
+        super(BinaryDistribution, self).__init__(2)
+
+
+DISTRIBUTIONS = dict(
+    # choice=ChoiceDistribution,
+    binary=BinaryDistribution,
+)
