bd9dda17b15a156ac39028675f118bb6a9026946
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-02-15 19:45:14 -0800

    adding d3 and nvd3 dependencies

---
+++ graphs.py
@@ -0,0 +1,31 @@
+import nvd3
+
+"""
+from nvd3 import pieChart
+
+type = 'pieChart'
+chart = pieChart(name=type, color_category='category20c', height=450, width=450)
+chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+
+#Create the keys
+xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple"]
+ydata = [3, 4, 0, 1, 5, 7, 3]
+
+#Add the serie
+extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
+chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
+chart.buildcontent()
+chart.htmlcontent
+
+"""
+
+def pie_chart(labels, data, name="", label=""):
+    chart = nvd3.pieChart(name=name,
+                          color_category='category20c',
+                          height=450, width=450)
+
+    #Add the serie
+    extra_serie = {"tooltip": {"y_start": "", "y_end": label}}
+    chart.add_serie(y=data, x=labels, extra=extra_serie)
+    chart.buildcontent()
+    return chart.htmlcontent
