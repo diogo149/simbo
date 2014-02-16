8740411c1ac4b4ea5c3e2bbe9d501f01686bcfb1
racoonsmakeokayfriends <amanda.knight314@gmail.com>
2014-02-16 12:10:54 -0800

    prettification

--- static/simbo.js
+++ static/simbo.js
@@ -1,7 +1,7 @@
 $(document).ready(function()    {
 
     var variables = [];
-    var num_distributions = 5;
+    var num_distributions = 1;
     var default_var = "";
     var distr_var = "";
 
@@ -59,7 +59,7 @@ $(document).ready(function()    {
         $('#application_div').show();
     });
 
-    ///// user clicked "new experiment" button
+    ///// user clicked "home" button
     $('#home_nav_btn').click(function()    {
         $('#application_div').hide();
         $('#home_div').show();
@@ -145,11 +145,9 @@ $(document).ready(function()    {
         update_list();
     });
 
-<<<<<<< HEAD
     build_variable_input_form();
     //$('.ui.dropdown').dropdown();
-;
-=======
+
 	$("#foo").click(function() {
 		$.get( "/foobar", function( data ) {
 			// $("#chart").html( data );
@@ -158,157 +156,6 @@ $(document).ready(function()    {
 			alert( "Load was performed." );
 		});
 	});
-
->>>>>>> 7eefd1e4a526e42709034aec4a7cca2ffea0946d
-/*
-    $('#init_round_btn').click(function()   {
-        var phrase_to_guess = $('input[name=phrase_to_guess]').val();
-        $('input[name=phrase_to_guess]').val('');
-        if (phrase_to_guess.length < 3) {
-            alert("Your phrase should be at least 3 characters long!");
-            return;
-        }
-        start_round_2(phrase_to_guess.toUpperCase());
-    });
-
-    $(document).on('click','.letter', function()  {
-        var guess = $(this).val().toUpperCase();
-        take_guess(guess);
-        $(this).hide();
-    });
-
-    $('#play_again_yes').click(function()    {
-        console.log('boogers');
-        $('#new_round').fadeOut('slow');
-        ctx.clearRect(0,0,canvas_w,canvas_h);
-        show_buttons();
-        round = start_round_1();
-    });
-
-    $('#play_again_no').click(function()    {
-        $('#new_round').fadeOut('slow');
-        $('#mandy_points').html("Mandy got: " + points["Mandy"] + " points");
-        $('#deedee_points').html("Deedee got: " + points["Deedee"] + " points");
-        $('#game_over').show();
-    });
-
-    // to do: implement round_lost()
-    function round_lost()    {
-        alert(round.host + " won!");
-        round.curr_array = round.answer_array;
-        points[round.host] += 1;
-        draw();
-        $('#title').html("You didn't save " + round.friend_name + " :(");
-        round_over();
-    };
-
-    function round_won()    {
-        alert(round.guesser + " won! " + round.friend_name + " has been saved!");
-        $('#title').html("You saved " + round.friend_name + "!");
-        points[round.guesser] += 1;
-        draw();
-        round_over();
-    };
-
-    function round_over()   {
-        $('img').attr("src",round.friend_gif);
-        $("#guess_ui").fadeOut('slow');
-        $("#new_round").show();
-        // you get more points for doing this better, but this will work
-        //$('#wrong_guesses').html("");
-        round = {};
-    };
-
-    // returns true if game is over, false if game is still on!
-    function wrong_guess(guess) {
-        round.wrong_guesses.push(guess);
-        //$('#wrong_guesses').html(array_to_str(round.wrong_guesses));
-        round.num_wrong++;
-        if (round.num_wrong >= 6)   {
-            round_lost();
-            return true;
-        }
-        return false;
-    };
-
-    function take_guess(guess)  {
-        if (already_guessed(guess)) {
-            alert("You already guessed this letter!");
-            return;
-        }
-
-        round.guesses.push(guess);
-        var ans = round.answer_array;
-        var correct = false;
-        for (var i = 0; i < ans.length; i++)    {
-            if (ans[i] === guess)   {
-                correct = true;
-                round.curr_array[i] = guess;
-            }
-        }
-
-        if (!correct)   {
-            // if game is over
-            if (wrong_guess(guess)) {
-                return;
-            }
-            draw();
-        }
-        else    {
-            if (has_won())    {
-                round_won();
-                return;
-            }
-        }
-
-        draw();
-    };
-
-    /////// gameplay functions ///////
-
-    function start_round_1()  {
-        var g = Math.floor(Math.random()*2);
-        // 0 for Mandy, 1 for Deedee
-
-        var f = Math.floor(Math.random()*8);
-
-        var r = {
-            guesser: g === 0 ? "Mandy" : "Deedee",
-            host: g !== 0 ? "Mandy" : "Deedee",
-            num_wrong: 0,
-            wrong_guesses: [],
-            guesses: [],
-            friend_img: all_friends[f],
-            friend_name: friends_names[f],
-            friend_gif: f_gifs[f]
-        };
-
-        var dir_text = r.guesser + ", look away while ";
-        dir_text += r.host + " enters a phrase.";
-        $('#init_round_dir').html(dir_text);
-        $('#init_round').show();
-
-        return r;
-    }
-
-    function start_round_2(phrase)    {
-        $('#init_round').fadeOut('slow');
-        $('#guess_ui').fadeIn('slow');
-        var dir_text = round.guesser + ', please guess a letter!';
-        $('#guess_dir').html(dir_text);
-        $('#title').html('You got to save ' + round.friend_name + '!');
-        init_phrase_arrays(phrase);
-        draw();
-    }
-
-    function show_buttons() {
-        alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z']
-        for (var i=0; i < 26; i++)  {
-            $('#'+alpha[i]).show();
-        }
-    }
-
-    */
 });
 
 /*
