711424c060ba96da0810ea9ee2cb1e9dc1631f86
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-02-15 20:19:54 -0800

    schema routes

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import flask
 import json
-
+import joblib
 
 import distributions
 
@@ -9,18 +9,37 @@ app = flask.Flask(__name__,
                   static_folder='static',
                   static_url_path='')
 
+### Utility functions
 
-@app.route('/', methods=['GET'])
-def homepage():
-    return flask.redirect(flask.url_for('static',
-                                        filename='index.html'))
-
-@app.route('/experiment/<uuid>', methods=['GET'])
-def show_experiment(uuid): # not necessary, use single page app
-    return "TODO"
+def schema_pkl(uuid):
+    return"data/{}_schema.pkl".format(uuid)
 
 
-@app.route('/experiment/sample/<uuid>', methods=['GET'])
+### Schema routes
+
+
+@app.route('/api/schema/<uuid>', methods=['GET'])
+def show_schema(uuid): # not necessary, use single page app
+    try:
+        schema = joblib.load(schema_pkl(uuid))
+    except:
+        schema = {}
+    return json.dumps(schema)
+
+
+@app.route('/api/schema/<uuid>', methods=['POST'])
+def add_to_schema(uuid):
+    data = flask.request.json
+    if not data:
+        flask.abort(400) # bad request
+    joblib.dump(data, schema_pkl(uuid))
+    return "Success"
+
+
+### Sample routes
+
+
+@app.route('/api/sample/<uuid>', methods=['GET'])
 def sample_experiment(uuid):
     # TODO take in json (for personalization)
     # TODO see if any precomputed settings are available
@@ -28,14 +47,22 @@ def sample_experiment(uuid):
     return "TODO"
 
 
-@app.route('/experiment/sample/<uuid>', methods=['POST'])
+@app.route('/api/sample/<uuid>', methods=['POST'])
 def experiment_results(uuid):
     data = flask.request.json
-    print data
     if not data:
         flask.abort(400) # bad request
     # TODO handle data
     return "Success"
+
+
+### Misc routes
+
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return flask.redirect(flask.url_for('static',
+                                        filename='index.html'))
 
 
 @app.route('/api/distribution_schema')
